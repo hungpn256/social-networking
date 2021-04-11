c1a5309cafdcaf2f5bcbbd9aaf505d6c477a0d29
Hung Pham <phamnanghung.25@gmail.com>
2021-04-12 00:34:54 +0700

    nav

--- src/Components/NotFound/index.tsx
+++ src/Components/NotFound/index.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Result, Button } from 'antd';
+import { useHistory } from 'react-router';
 
 const NotFound = () => {
-  return <div style={{ textAlign: 'center', color: 'red', fontSize: 30 }}>404 - NOT FOUND</div>;
+  const history = useHistory();
+  return (
+    <div style={{ textAlign: 'center', color: 'red', fontSize: 30 }}>
+      {' '}
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button
+            type="primary"
+            onClick={() => {
+              history.push('/');
+            }}
+          >
+            Back Home
+          </Button>
+        }
+      />
+      ,
+    </div>
+  );
 };
 
 export default NotFound;
