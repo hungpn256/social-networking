22569ec2347d332060d184d846f1adae8cd3e0d0
Hung Pham <phamnanghung.25@gmail.com>
2021-04-12 11:06:57 +0700

    fix footer,  header

---
+++ src/Components/NavBarHeader/index.tsx
@@ -0,0 +1,124 @@
+import {
+  DownCircleTwoTone,
+  HomeFilled,
+  LoginOutlined,
+  SettingOutlined,
+  UnorderedListOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Dropdown, Input, Menu } from 'antd';
+import Avatar from 'antd/lib/avatar/avatar';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { NavLink, Route, Switch } from 'react-router-dom';
+import Logo from '../../Assets/logo.png';
+import styles from './styles.module.css';
+
+const { Search } = Input;
+const menu = (
+  <Menu>
+    <Menu.Item key="0">
+      <a href="/">
+        <SettingOutlined />
+        Setting
+      </a>
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item
+      key="3"
+      onClick={() => {
+        localStorage.removeItem('token');
+        window.open('auth/login', '_self');
+      }}
+    >
+      <LoginOutlined />
+      Log out
+    </Menu.Item>
+  </Menu>
+);
+export default function Home(props: any) {
+  const [current, setCurrent] = useState('mail');
+  const user = useSelector((state) => state.login.user);
+  console.log(user.avatar.viewUrl, 'user');
+  const handleClick = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  const onSearch = (value: string) => console.log(value);
+  return (
+    <>
+      <div style={{ boxShadow: '0 2px 2px 0 rgba(0, 0, 0, 0.2)', backgroundColor: 'pink' }}>
+        <div className={styles['header']}>
+          <nav className={styles['header-nav']} id="navbar">
+            <div className={styles['max-width']}>
+              <input type="checkbox" id={styles['check']}></input>
+              <label className={styles['checkbtn']} htmlFor={styles['check']}>
+                <UnorderedListOutlined />
+              </label>
+              <label className={styles['brand']}>
+                <img
+                  style={{ position: 'relative' }}
+                  className={styles['logo']}
+                  src={Logo}
+                  alt="Logo"
+                ></img>
+                <Search
+                  placeholder="Search"
+                  allowClear
+                  size="large"
+                  onSearch={onSearch}
+                  style={{ width: 250 }}
+                />
+              </label>
+              <ul className={styles['menu']}>
+                <li className={styles['menu-item']}>
+                  <NavLink
+                    to="/"
+                    className={styles['menu-item-link']}
+                    activeClassName={styles['active']}
+                  >
+                    Home
+                    <HomeFilled style={{ marginLeft: 8 }} />
+                  </NavLink>
+                </li>
+                <li className={styles['menu-item']}>
+                  <NavLink
+                    to={`/profile/${user._id}`}
+                    className={`${styles['menu-item-link']}`}
+                    activeClassName={styles['active']}
+                  >
+                    <span>{`${user.name.firstName} ${user.name.lastName}`}</span>
+                    <Avatar
+                      icon={<UserOutlined />}
+                      style={{ marginLeft: 4 }}
+                      src={user && user?.avatar.viewUrl}
+                    />
+                  </NavLink>
+                </li>
+                <li
+                  className={styles['menu-item']}
+                  style={{
+                    margin: 0,
+                    padding: '0',
+                  }}
+                >
+                  <Dropdown overlay={menu} trigger={['click']}>
+                    <DownCircleTwoTone
+                      style={{
+                        fontSize: 25,
+                        lineHeight: '30px',
+                        transform: 'translate(-5px, 6px)',
+                      }}
+                      twoToneColor="pink"
+                    />
+                  </Dropdown>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,45 +1,11 @@
-import {
-  DownCircleTwoTone,
-  HomeFilled,
-  LoginOutlined,
-  SettingOutlined,
-  UnorderedListOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
-import { Dropdown, Input, Menu } from 'antd';
-import Avatar from 'antd/lib/avatar/avatar';
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { NavLink, Route, Switch } from 'react-router-dom';
-import Logo from '../../Assets/logo.png';
-import styles from './styles.module.css';
+import NavBarHeader from '../../Components/NavBarHeader';
 
-const { Search } = Input;
-const menu = (
-  <Menu>
-    <Menu.Item key="0">
-      <a href="/">
-        <SettingOutlined />
-        Setting
-      </a>
-    </Menu.Item>
-    <Menu.Divider />
-    <Menu.Item
-      key="3"
-      onClick={() => {
-        localStorage.removeItem('token');
-        window.open('auth/login', '_self');
-      }}
-    >
-      <LoginOutlined />
-      Log out
-    </Menu.Item>
-  </Menu>
-);
 export default function Home(props: any) {
   const [current, setCurrent] = useState('mail');
   const user = useSelector((state) => state.login.user);
-  console.log(user, 'user');
+  console.log(user.avatar.viewUrl, 'user');
   const handleClick = (e) => {
     console.log('click ', e);
     setCurrent(e.key);
@@ -48,79 +14,7 @@ export default function Home(props: any) {
   const onSearch = (value: string) => console.log(value);
   return (
     <>
-      <div style={{ boxShadow: '0 2px 2px 0 rgba(0, 0, 0, 0.2)', backgroundColor: 'pink' }}>
-        <div className={styles['header']}>
-          <nav className={styles['header-nav']} id="navbar">
-            <div className={styles['max-width']}>
-              <input type="checkbox" id={styles['check']}></input>
-              <label className={styles['checkbtn']} htmlFor={styles['check']}>
-                <UnorderedListOutlined />
-              </label>
-              <label className={styles['brand']}>
-                <img
-                  style={{ position: 'relative' }}
-                  className={styles['logo']}
-                  src={Logo}
-                  alt="Logo"
-                ></img>
-                <Search
-                  placeholder="Search"
-                  allowClear
-                  size="large"
-                  onSearch={onSearch}
-                  style={{ width: 200 }}
-                />
-              </label>
-              <ul className={styles['menu']}>
-                <li className={styles['menu-item']}>
-                  <NavLink
-                    to="/"
-                    className={styles['menu-item-link']}
-                    activeClassName={styles['active']}
-                  >
-                    Home
-                    <HomeFilled style={{ marginLeft: 8 }} />
-                  </NavLink>
-                </li>
-                <li className={styles['menu-item']}>
-                  <NavLink
-                    to={`/profile/${user._id}`}
-                    className={`${styles['menu-item-link']}`}
-                    activeClassName={styles['active']}
-                  >
-                    <span>{`${user.name.firstName} ${user.name.lastName}`}</span>
-                    <Avatar icon={<UserOutlined />} style={{ marginLeft: 4 }} />
-                  </NavLink>
-                </li>
-                <li
-                  className={styles['menu-item']}
-                  style={{
-                    margin: 0,
-                    padding: '0',
-                  }}
-                >
-                  <Dropdown overlay={menu} trigger={['click']}>
-                    <DownCircleTwoTone
-                      style={{
-                        fontSize: 25,
-                        lineHeight: '30px',
-                        transform: 'translate(-5px, 6px)',
-                      }}
-                      twoToneColor="pink"
-                    />
-                  </Dropdown>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </div>
-      </div>
-      <Switch>
-        <Route path="/profile/606dda78e0506b0015f824e4">Profile</Route>
-        <Route path="/" exact>
-          Home
-        </Route>
-      </Switch>
+      <NavBarHeader />
     </>
   );
 }
