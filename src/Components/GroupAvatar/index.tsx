b73faee36a2f1efdc8e7bfe2131347d312d96849
Hung Pham <phamnanghung.25@gmail.com>
2022-09-13 21:54:59 +0700

    avatar

--- src/Components/GroupAvatar/index.tsx
+++ src/Components/GroupAvatar/index.tsx
@@ -7,8 +7,20 @@ export default function GroupAvatar({ src, style, size }: { src: (string | undef
   return (
     <div style={style} className={styles['group-avatar']}>
       {src.map((i) => {
-        return <div className={src.length === 1 ? styles['avatar-wrapper'] : styles['avatar-wrapper-group']}>
-          <Avatar style={{ width: size || 50, height: size || 50 }} icon={<UserOutlined />} src={i} className={src.length === 1 ? styles['avatar-item'] : styles['avatar-item-group']} />
+        return <div className={src.length === 1 ?
+          styles['avatar-wrapper'] :
+          styles['avatar-wrapper-group']}>
+          <Avatar style={
+            {
+              width: src.length === 1 ? size || 50 : (size || 50) * 0.75,
+              height: src.length === 1 ? size || 50 : (size || 50) * 0.75
+            }
+          }
+            icon={<UserOutlined />}
+            src={i}
+            className={src.length === 1 ?
+              styles['avatar-item'] :
+              styles['avatar-item-group']} />
         </div>
       })}
     </div >
--- src/Helper/Chat.ts
+++ src/Helper/Chat.ts
@@ -12,6 +12,7 @@ export const getNameMessage = (conversation: IConversation, user: IUser): string
 };
 
 export const getAvatarMessage = (conversation: IConversation, user: IUser): (string | undefined)[] => {
+  console.log("🚀 ~ file: Chat.ts ~ line 19 ~ getAvatarMessage ~ conversation.participants", conversation.participants.filter((item) => item.user._id !== user._id).slice(0, 2).map((i) => i.user.avatar))
   return conversation.participants
     .filter((item) => item.user._id !== user._id)
     .slice(0, 2)
