9bc4f16eb6357eeff45de2c03104f4cb52c9a0c5
Hung Pham <phamnanghung.25@gmail.com>
2022-12-03 21:43:21 +0700

    ip

--- src/configs/ip.js
+++ src/configs/ip.js
@@ -1,9 +1,7 @@
 // const ip = 'https://social-networking-v1.herokuapp.com/api/v1';
 const environment = process.env.NODE_ENV || 'development';
 const ip =
-  environment === 'development'
-    ? 'http://localhost:8000/api/v1'
-    : 'https://do-an-social.herokuapp.com/api/v1';
+  environment === 'development' ? 'http://localhost:8000/api/v1' : 'https://2dl1ul.deta.dev/api/v1';
 const ipSocket =
-  environment === 'development' ? 'http://localhost:8000/' : 'https://do-an-social.herokuapp.com/';
+  environment === 'development' ? 'http://localhost:8000/' : 'https://2dl1ul.deta.dev/';
 export { ip, ipSocket };
