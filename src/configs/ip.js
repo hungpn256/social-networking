1b847abde6d095fe4f613ced200f25a5be737093
Hung Pham <phamnanghung.25@gmail.com>
2023-02-11 23:06:17 +0700

    ip

--- src/configs/ip.js
+++ src/configs/ip.js
@@ -6,6 +6,6 @@ const ip =
     : 'https://social-backend-tzwbd.appengine.bfcplatform.vn/api/v1';
 const ipSocket =
   environment === 'development'
-    ? 'http://localhost:8000/'
-    : 'https://social-backend-tzwbd.appengine.bfcplatform.vn/';
+    ? 'http://localhost:8000'
+    : 'https://social-backend-tzwbd.appengine.bfcplatform.vn';
 export { ip, ipSocket };
