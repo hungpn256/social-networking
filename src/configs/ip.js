e9547a703d01accf3660ddaf524bfb0a21dbe3a0
Hung Pham <phamnanghung.25@gmail.com>
2023-02-11 23:00:16 +0700

    ip

--- src/configs/ip.js
+++ src/configs/ip.js
@@ -1,7 +1,11 @@
 // const ip = 'https://social-networking-v1.herokuapp.com/api/v1';
 const environment = process.env.NODE_ENV || 'development';
 const ip =
-  environment === 'development' ? 'http://localhost:8000/api/v1' : 'https://2dl1ul.deta.dev/api/v1';
+  environment === 'development'
+    ? 'http://localhost:8000/api/v1'
+    : 'https://social-backend-tzwbd.appengine.bfcplatform.vn/api/v1';
 const ipSocket =
-  environment === 'development' ? 'http://localhost:8000/' : 'https://2dl1ul.deta.dev/';
+  environment === 'development'
+    ? 'http://localhost:8000/'
+    : 'https://social-backend-tzwbd.appengine.bfcplatform.vn/';
 export { ip, ipSocket };
