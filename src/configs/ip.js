3385f45fd5d09640aad69a4fc55b6dab0ccd4697
HungPN <hungpn@HungPN-D8-M.local>
2023-05-12 16:00:54 +0700

    ip

--- src/configs/ip.js
+++ src/configs/ip.js
@@ -3,9 +3,7 @@ const environment = process.env.NODE_ENV || 'development';
 const ip =
   environment === 'development'
     ? 'http://localhost:8000/api/v1'
-    : 'https://social-backend-tzwbd.appengine.bfcplatform.vn/api/v1';
+    : 'https://social-be-d4n8.onrender.com/api/v1';
 const ipSocket =
-  environment === 'development'
-    ? 'http://localhost:8000'
-    : 'https://social-backend-tzwbd.appengine.bfcplatform.vn';
+  environment === 'development' ? 'http://localhost:8000' : 'https://social-be-d4n8.onrender.com';
 export { ip, ipSocket };
